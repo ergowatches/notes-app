71ddb946c4d76bf901735420d0466df95f041e28
Asmit Agrawal <asmitagrawal@Asmits-MBP.broadband>
2024-11-19 18:40:00 +0000

    Updated app with Sidebar and NoteEditor

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,10 +4,8 @@ interface LayoutProps {
   children: React.ReactNode;
 }
 
-export const Layout: React.FC<LayoutProps> = ({ children }) => {
-  return (
-    <div className="fixed inset-0 flex w-full h-full overflow-hidden">
-      {children}
-    </div>
-  );
-};
+export const Layout: React.FC<LayoutProps> = ({ children }) => (
+  <div className="flex h-screen bg-white overflow-hidden">
+    {children}
+  </div>
+);
